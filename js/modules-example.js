9ae8c2beba354960007d5c744ceefe93a3dfd38b
sonu yadav <sonuyadav94220@gmail.com>
2021-01-10 13:08:25 +0530

    new changes

---
+++ js/modules-example.js
@@ -0,0 +1,30 @@
+//
+// function show (x, y){
+//     let result = 0;
+//     if (x===y){
+//         result =  (x+y)*10;
+//     }else {
+//         result = x + y;
+//     }
+//     console.log(result);
+// }
+//
+// show(10, 10);
+//
+// show(10,3)
+
+
+
+function show (x, y){
+    let result = 0
+    if (x===y){
+        result = x + y;
+    }else {
+        result = (x+y)*10;
+    }
+    console.log(result);
+}
+
+show(10, 10);
+
+show(10,3)
