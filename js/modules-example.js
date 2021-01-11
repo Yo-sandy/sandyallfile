bdb5a06405d916e06576214ecbad0f65b38cfc95
sonu yadav <sonuyadav94220@gmail.com>
2021-01-11 11:27:07 +0530

    some changes

--- js/modules-example.js
+++ js/modules-example.js
@@ -14,20 +14,52 @@
 // show(10,3)
 
 
+//
+// function show (x, y){
+//     let result = 0
+//     if (x===y){
+//         result = x + y;
+//     }else {
+//         result = (x+y)*10;
+//     }
+//     console.log(result);
+// }
+//
+// show(10, 10);
+//
+// show(10,3)
+//
+//
+// show(10,5)
 
-function show (x, y){
-    let result = 0
-    if (x===y){
-        result = x + y;
-    }else {
-        result = (x+y)*10;
+
+//
+// function  show (x){
+//     let result = 0;
+//     let y = 51;
+//     if (x < y){
+//         result = y - x;
+//     }else  {
+//         result = (x-y)*2;
+//     }
+//     console.log(result);
+// }
+//
+// show(10)
+//
+// show(60)
+
+ function  show (x){
+    let result = 0;
+    let  y = 100;
+    if (x < y){
+        result = false
+    }else  {
+        result = true
     }
-    console.log(result);
-}
+     console.log(result)
+ }
 
-show(10, 10);
+ show(101)
 
-show(10,3)
-
-
-show(10,5)
+show(99)
